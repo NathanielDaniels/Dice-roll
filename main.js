977573ee478c02acf1ae50e001695dd471f1bb49
NathanielDaniels <26263599+NathanielDaniels@users.noreply.github.com>
2020-12-11 23:02:26 -0600

    added readme

--- main.js
+++ main.js
@@ -1,13 +1,4 @@
-/*
-DESCRIPTION:
-In this challenge a casino has asked you to make an online dice that works just like 
-it would in real life. Using the pre-made dice face that represents ‘one’, make the 
-faces for ‘two’, ‘three’, ‘four’, ‘five’ and ‘six’. Now when the users clicks the 
-dice on the screen the dice is expected to show one of the faces randomly.
 
-event listeners, Math.random()
-
-*/
 
 let diceRoll = () => {
     const dice = document.querySelector('.dice')
